0b3bc4a0d6996b9ce66e84bc3b0c3897cc8aa847
nurekaht <nurekaht@student.ub.ac.id>
2020-11-09 06:53:01 +0700

    test

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use \Iluminate\Http\Request;
+use \Iluminate\Http\Response;
+use Iluminate\Database\Eloquent\ModelNotFoundException;
+
+class BooksController extends Controller
+{
+    public function index()
+    {
+        return Book::all();
+    }
+
+    public function Id($id)
+    {
+        $book = Book::where('id', $id)->first();
+        if ($book) {
+            return $book;
+        } else {
+            return response()->json([
+                'message' => 'Book Not Found',
+            ], 404);
+        }
+    }
+}
